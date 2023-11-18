885ad54df35d011a4d830306a61922131045468b
Abanoub-Gamal <148594952+Abanoub-Gamal@users.noreply.github.com>
2023-11-18 17:29:47 +0200

    Update p.php

--- p.php
+++ p.php
@@ -3,8 +3,9 @@ require_once 'php.php';
 $query="select * from users";
 $result=$conn->query($query);
 $out=[];
+header("Content-Type: JSON");
 while($item = $result->fetch_assoc()){
     $out[]=$item;
 
 }
-echo json_encode($out);
+echo json_encode($out,  JSON_PRETTY_PRINT);
